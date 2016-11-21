2cd1fc118141a1326d50dde2fae0a232d3a4bfb7
pvo125 <pvo125@gmail.com>
2016-11-21 22:57:22 +0700

    1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -118,7 +118,7 @@ void MainWindow::on_Disconnect_clicked()
 
 }
 
-void MainWindow::on_File_clicked()          //ghju  gvnjvbm
+void MainWindow::on_File_clicked()          //при нажатии на кнопку File.. открываем диалог по выбору файла бинарника
 {
     QString filename=QFileDialog::getOpenFileName(this,tr("Open file"),"D:\\STM32_проекты","All Files (*.*);;Binary files (*.bin);;Text files (*.txt)");
 
