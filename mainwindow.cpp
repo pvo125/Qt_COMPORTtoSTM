f5d772ad64dbaea220a10adf9133a7e2bb65befd
pvo125 <pvo125@gmail.com>
2016-11-28 08:17:12 +0700

    8:16_281116

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -257,7 +257,7 @@ void MainWindow::RecieveBytes()
             ui->Load_to_STM->setEnabled(true);          // кнопка Load_to_STM активна
             ui->Disconnect->setEnabled(false);          // кнопка Disconnect  не активна
             QMessageBox::information(this,tr("Message"),"CRC ERROR!");  // выводим сообщение об ошибки контрольной суммы
-         }
+         }  //privet
 
 }
 
